4d5ec8f732f2a57eb9bb2014cd7ea7c1f099ad8c
phpwebdev <phpwebdev@gmaIl.com>
2016-04-13 23:13:15 +0300

    test
    test

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
      */
 
-    'url'             => env('APP_URL', 'http://dobri-server.loc/'),
+    'url'             => env('APP_URL', 'http://dobri-server.loc/' ),
 
     /*
     |--------------------------------------------------------------------------
