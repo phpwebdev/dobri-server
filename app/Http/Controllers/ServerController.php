ee8d16f61c7c1cb3c2ad804d70d45a013e6aa642
phpwebdev <phpwebdev@gmaIl.com>
2016-05-07 01:37:55 +0300

    Added middleware on Server controler

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -14,7 +14,10 @@ use App\Http\Requests\CreateServerRequest;
 class ServerController extends Controller
 {
 
-    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     /**
      * Display a listing of the resource.
